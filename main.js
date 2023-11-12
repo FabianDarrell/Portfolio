7d881a15b10a1030bb0746d5068ff6379ba9c6f9
Fabian <fabian.535210062@stu.untar.ac.id>
2023-11-12 10:30:44 +0700

    Step1

---
+++ main.js
@@ -0,0 +1,28 @@
+//navbar-fixed
+window.onscroll = function () {
+    const header = document.querySelector("header");
+    const fixedNav = header.offsetTop;
+  
+    if (window.pageYOffset > fixedNav) {
+      header.classList.add("navbar-fixed");
+    } else {
+      header.classList.remove("navbar-fixed");
+    }
+  };
+  
+  //hamburger menu
+  const hamburger = document.querySelector("#hamburger");
+  const navMenu = document.querySelector("#nav-menu");
+  
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("hamburger-active");
+    navMenu.classList.toggle("hidden");
+  });
+  
+  //Outside hamburger menu click
+  window.addEventListener("click", (e) => {
+    if (e.target != hamburger && e.target != navMenu) {
+      hamburger.classList.remove("hamburger-active");
+      navMenu.classList.add("hidden");
+    }
+  });
